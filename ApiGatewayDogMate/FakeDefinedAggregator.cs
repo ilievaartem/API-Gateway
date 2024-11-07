ec205fb3f9830746d9868c4bee7a16216254c85d
Iliev Artem <aartemiliev@gmail.com>
2024-11-07 07:44:10 +0200

    First commit

---
+++ ApiGatewayDogMate/FakeDefinedAggregator.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using System.Text;
+using Ocelot.Middleware;
+using Ocelot.Multiplexer;
+
+namespace ApiGatewayDogMate;
+
+public class FakeDefinedAggregator : IDefinedAggregator
+{
+    public async Task<DownstreamResponse> Aggregate(List<HttpContext> responses)
+    {
+        var content = new List<string>();
+
+        foreach (var response in responses)
+        {
+            var downstreamContent = await response.Items.DownstreamResponse().Content.ReadAsStringAsync();
+            content.Add(downstreamContent);
+        }
+
+        var aggregatedContent = $"[{string.Join(",", content)}]";
+        var stringContent = new StringContent(aggregatedContent, Encoding.UTF8, "application/json");
+
+        return new DownstreamResponse(stringContent, HttpStatusCode.OK,
+            new List<KeyValuePair<string, IEnumerable<string>>>(), "application/json");
+    }
+}
---
+++ ApiGatewayDogMate/Startup.cs
@@ -0,0 +1,25 @@
+﻿using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace ApiGatewayDogMate;
+
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddOcelot().AddSingletonDefinedAggregator<FakeDefinedAggregator>();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseRouting();
+        app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        app.UseOcelot().Wait();
+    }
+    
+}
